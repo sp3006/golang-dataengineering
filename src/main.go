57024b51a12300841d79df1830bb028e6ea7b420
SushantParicharak <sushant.paricharak@gmail.com>
2021-12-27 14:34:03 -0500

    Update main.go

--- src/main.go
+++ src/main.go
@@ -9,7 +9,12 @@ func main(){
   // The "HandleFunc" method accepts a path and a function as arguments
   // (Yes, we can pass functions as arguments, and even treat them like variables in Go)
  // However, the handler function has to have the appropriate signature (as described by the "handler" function below)
-   http.HandleFunc("/", handler
+   http.HandleFunc("/", handler)
+
+   // After defining our server, we finally "listen and serve" on port 8080
+	// The second argument is the handler, which we will come to later on, but for now it is left as nil,
+	// and the handler defined above (in "HandleFunc") is used
+	http.ListenAndServe(":8080", nil)
 }
 
 
